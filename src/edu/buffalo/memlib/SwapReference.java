a47317c03cd84d0f040927494c9c08355610a6ea
Brandon Ross <bwross@buffalo.edu>
2014-11-12 11:40:40 -0500

    Adding preliminary SwapReference.

--- src/edu/buffalo/memlib/SwapReference.java
+++ src/edu/buffalo/memlib/SwapReference.java
@@ -2,186 +2,76 @@
 
 import java.io.*;
 import java.lang.ref.*;
+import java.util.*;
 
-public class SwapReference<T extends Serializable> {
-  private static final ReferenceQueue<Serializable> queue =
-    new ReferenceQueue<Serializable>();
-  private static final SwapFile swap = SwapFile.instance();
+/** A swappable reference to an object. */
+public class SwapReference<T> {
+  /**
+   * The referent or swap token. This will either be a SwapToken object, or
+   * something else (including null).
+   */
+  private T object;
 
-  /** Thread which swaps objects when they're being freed. */
-  private static final Thread swapThread = new Thread("swap-queue-thread") {
-    {
-      setDaemon(true);
-      start();
-    }
+  /** Create a SwapReference with a null referent. */
+  public SwapReference() { this(null); }
 
-    public void run() {
-      while (true) try {
-        InternalReference ref = (InternalReference) queue.remove();
-        ref.swapOut();
-      } catch (InterruptedException e) {
-        // This will never happen...
-      }
-    }
-  };
+  /** Create a SwapReference referring to the given object. */
+  public SwapReference(T object) { this.object = object; }
 
-  private InternalReference<T> ref;
-
-  private interface InternalReference<T1> {
-    T1 get();
-    void free();
-    void swapIn();
-    void swapOut();
-  }
-
-  /** A reference to an object still on the heap. */
-  private final class LiveReference extends SoftReference<T>
-  implements InternalReference<T> {
-    private boolean freed = false;
-
-    LiveReference(T object) { super(object, queue); }
-
-    public void free() {
-      // Synchronized - called in finalizer.
-      if (!freed) synchronized (SwapReference.this) {
-        clear();
-        freed = true;
-      }
-    }
-
-    public void swapIn() { }
-
-    public void swapOut() {
-      // Synchronized - called in swapThread.
-      if (!freed) synchronized (SwapReference.this) {
-        try {
-          T t = get();
-          clear();
-          long token = swap().put(t);
-          ref = new DeadReference(token);
-        } catch (Exception e) {
-          e.printStackTrace();
-          SwapReference.this.clear();
-        }
-      }
-    }
-
-    public void finalize() { free(); }
-  }
-
-  /** A reference to an object which has been swapped out. */
-  private final class DeadReference implements InternalReference<T> {
-    private long token;
-
-    DeadReference(long token) { this.token = token; }
-
-    public T get() {
-      if (token < 0) {
-        return null;
-      } try {
-        T t = (T) swap().get(token);
-        free();
-        return t;
-      } catch (Exception e) {
-        e.printStackTrace();
-        return null;
-      }
-    }
-
-    public void free() {
-      // Synchronized - called in finalizer.
-      synchronized (SwapReference.this) {
-        swap().free(token);
-        token = -1;
-      }
-    }
-
-    public void swapIn() {
-      if (token < 0) {
-        SwapReference.this.clear();
-      } else try {
-        set((T) swap().get(token));
-      } catch (Exception e) {
-        e.printStackTrace();
-        clear();
-      }
-    }
-
-    public void swapOut() { }
-  }
-
-  /** Used when this SwapReference is set to null. */
-  private final class NullReference implements InternalReference<T> {
-    public T get() { return null; }
-    public void free() { }
-    public void swapIn() { }
-    public void swapOut() { }
+  /**
+   * Get the referent. If it is swapped out, it will be swapped in.
+   */
+  public synchronized T get() {
+    if (isSwappedOut())
+      swapIn();
+    return object;
   }
 
   /**
-   * Creates a new swap reference that refers to the given object. The object
-   * is serialized and written to permanent storage when the garbage collector
-   * is has determined it should be freed.
-   *
-   * @param object object the swap reference will refer to
+   * Set the referent. If the current referent is swapped out, its swap slot
+   * will be freed.
    */
-  public SwapReference(T object) {
-    set(object);
-  }
-
-  /** Set this reference object's referent. */
   public synchronized void set(T object) {
-    if (ref != null)
-      ref.free();
-    ref = (object == null) ?
-      new NullReference() : new LiveReference(object);
+    if (isSwappedOut())
+      Swapper.free(token());
+    return object;
   }
 
-  /** Returns this reference object's referent. */
-  public synchronized T get() { return ref.get(); }
+  /** Bring the referent in from swap. No effect if already swapped in. */
+  public synchronized void swapIn() {
+    if (isSwappedOut())
+      object = Swapper.get(token());
+  }
 
-  /** Clears this reference object. */
-  public synchronized void clear() { set(null); }
+  /** Swap the referent out. No effect if already swapped out. */
+  public synchronized void swapOut() {
+    if (object != null && !isSwappedOut())
+      object = (T) Swapper.out(object);
+  }
 
-  /** Force the referent to be swapped out. */
-  public synchronized void swapOut() { ref.swapOut(); }
-
-  /** Swap the referent back in. */
-  public synchronized void swapIn() { ref.swapIn(); }
+  /** Returns whether or not the referent is in swap. */
+  private synchronized boolean isSwappedOut() {
+    return object instanceof SwapToken;
+  }
 
   /**
-   * Get the swap file used by this reference. By default, it's the global swap
-   * file. Subclasses can override this to use a different swap file.
+   * Get the swap token if we have one.
+   *
+   * @throws IllegalStateException If the referent is swapped in.
    */
-  SwapFile swap() { return SwapFile.instance(); }
-
-  public void finalize() { ref.free(); }
-
-  public static void main(String[] args) {
-    final int mb = 32;
-    SwapReference reference = new SwapReference(
-      new java.util.LinkedList<byte[]>() {{
-        for (int i = 0; i < mb; i++) add(new byte[1<<20]);
-      }}
-    );
-
-    System.out.println(reference.ref);
-    System.out.println(Runtime.getRuntime().freeMemory());
-    System.out.println();
-
-    while (true) {
-      reference.swapOut();
-      System.gc();
-
-      System.out.println(reference.ref);
-      System.out.println(Runtime.getRuntime().freeMemory());
-      System.out.println();
-
-      reference.swapIn();
-      System.gc();
-      System.out.println(reference.ref);
-      System.out.println(Runtime.getRuntime().freeMemory());
-      System.out.println("---------");
-    }
+  private SwapToken<T> token() {
+    if (!isSwappedOut())
+      throw new IllegalStateException();
+    return (SwapToken<T>) object;
   }
+}
+
+/**
+ * A token which can be used to recover a swapped object. This class will be
+ * moved elsewhere later. For now, it's here only to demonstrate the concept.
+ */
+class SwapToken<T> {
+  private long token;
+
+  SwapToken(long token) { this.token = token; }
 }
