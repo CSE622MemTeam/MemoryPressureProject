401c4bc3a5e5bec017d25186d5889fda94b6a738
Alexander Simeonov <agsimeon@buffalo.edu>
2014-11-16 02:44:04 -0500

    Updates to SwapVector

--- src/edu/buffalo/memlib/util/SwapVector.java
+++ src/edu/buffalo/memlib/util/SwapVector.java
@@ -2,6 +2,7 @@ package edu.buffalo.memlib.util;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 import java.util.RandomAccess;
 import java.util.Vector;
@@ -15,39 +16,46 @@ public class SwapVector<E extends Serializable> extends AbstractList<E>
 														   RandomAccess, 
 														   Cloneable, 
 														   java.io.Serializable {
+	private static final long serialVersionUID = 2186400141782442375L;
 	private SwapReference<Vector<E>> ref;
 	
 	public SwapVector() {
 		ref = new SwapReference<Vector<E>>(new Vector<E>());
 	}
 	
+	public SwapVector(Collection<? extends E> c) {
+		ref = new SwapReference<Vector<E>>(new Vector<E>(c));
+	}
+	
+	public SwapVector(int initialCapacity) {
+		ref = new SwapReference<Vector<E>>(new Vector<E>(initialCapacity));
+	}
+	
+	public SwapVector(int initialCapacity, int capacityIncrement) {
+		ref = new SwapReference<Vector<E>>(new Vector<E>(initialCapacity, capacityIncrement));
+	}
+    
+    public boolean add(E e) {
+    	return ref.get().add(e);
+    }
+	
     @Override
     public E get(int location) {
-    	
-	// TODO Auto-generated method stub
-	return null;
+    	return ref.get().get(location);
     }
 
     @Override
     public int size() {
-	// TODO Auto-generated method stub
-	return 0;
-    }
-    
-    public boolean add(E e) {
-	
-	return true;
+    	return ref.get().size();
     }
 
     @Override
-    public boolean swap() {
-	// TODO Auto-generated method stub
-	return false;
+    public void swapIn() {
+    	ref.swapOut();
     }
 
     @Override
-    public boolean unswap() {
-	// TODO Auto-generated method stub
-	return false;
+    public void swapOut() {
+    	ref.swapOut();
     }
 }
