056978d107b72eec8791675e48705169abfb5148
Alexander Simeonov <agsimeon@buffalo.edu>
2014-11-16 13:58:23 -0500

    Polished SwapUtil and added commit.

--- src/edu/buffalo/memlib/swap/SwapUtil.java
+++ src/edu/buffalo/memlib/swap/SwapUtil.java
@@ -14,6 +14,21 @@ import android.os.Environment;
 public final class SwapUtil {
 	public static final String FILE_PREPEND = "edu.buffalo.swap.";
 	
+	/**
+	 * Deletes a swap file on internal storage.
+	 * 
+	 * @param id - file id
+	 */
+	public static void deleteFile(long id) {
+		deleteFile(id, true);
+	}
+	
+	/**
+	 * Deletes a swap file on selected medium.
+	 * 
+	 * @param id - file id
+	 * @param internal - true for internal storage, otherwise external
+	 */
 	public static void deleteFile(long id, boolean internal) {
 		Context context = SwapActivity.getSwapContext();
 		if (internal) {
@@ -24,89 +39,131 @@ public final class SwapUtil {
 			file.delete();
 		}
 	}
+	
+	/**
+	 * Acquires the swap file output stream on internal storage.
+	 * 
+	 * @param id - file id
+	 * @return the swap file output stream on internal storage
+	 * @throws FileNotFoundException
+	 */
+	public static FileOutputStream getFileOutputStream(long id) throws FileNotFoundException {
+		return getFileOutputStream(id, true);
+	}
 
-	public static FileOutputStream getFileOutputStream(long id, boolean internal) {
+	/**
+	 * Acquires the swap file output stream on selected medium.
+	 * 
+	 * @param id - file id
+	 * @param internal - true for internal storage, otherwise external
+	 * @return the swap file output stream on selected medium
+	 * @throws FileNotFoundException
+	 */
+	public static FileOutputStream getFileOutputStream(long id, boolean internal) throws FileNotFoundException {
 		Context context = SwapActivity.getSwapContext();
 		if (internal) {
-			try {
-				return context.openFileOutput(FILE_PREPEND + id, Context.MODE_PRIVATE);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
+			return context.openFileOutput(FILE_PREPEND + id, Context.MODE_PRIVATE);
 		}
 		else {
 			if (isExternalStorageWritable()) {
 				File file = new File(context.getExternalFilesDir(null), FILE_PREPEND + id);
-				try {
-					return new FileOutputStream(file);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					return null;
-				}
+				return new FileOutputStream(file);
 			}
 			else {
 				return null;
 			}
 		}
 	}
+	
+	/**
+	 * Acquires the swap file input stream on internal storage.
+	 * 
+	 * @param id - file id
+	 * @return the swap file input stream on internal storage
+	 * @throws FileNotFoundException
+	 */
+	public static FileInputStream getFileInputStream(long id) throws FileNotFoundException {
+		return getFileInputStream(id, true);
+	}
 
-	public static FileInputStream getFileInputStream(long id, boolean internal) {
+	/**
+	 * Acquires the swap file input stream on selected medium.
+	 * 
+	 * @param id - file id
+	 * @param internal - true for internal storage, otherwise external
+	 * @return the swap file input stream on selected medium
+	 * @throws FileNotFoundException
+	 */
+	public static FileInputStream getFileInputStream(long id, boolean internal) throws FileNotFoundException {
 		Context context = SwapActivity.getSwapContext();
 		if (internal) {
-			try {
-				return context.openFileInput(FILE_PREPEND + id);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
+			return context.openFileInput(FILE_PREPEND + id);
 		}
 		else {
 			if (isExternalStorageWritable()) {
 				File file = new File(context.getExternalFilesDir(null), FILE_PREPEND + id);
-				try {
-					return new FileInputStream(file);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					return null;
-				}
+				return new FileInputStream(file);
 			}
 			else {
 				return null;
 			}
 		}
 	}
+	
+	/**
+	 * Acquires the swap file object output stream on internal storage.
+	 * 
+	 * @param id - file id
+	 * @return the swap file object output stream on internal storage
+	 * @throws FileNotFoundException
+	 */
+	public static ObjectOutputStream getObjectOutputStream(long id) throws IOException {
+		return getObjectOutputStream(id, true);
+	}
 
-	public static ObjectOutputStream getObjectOutputStream(long id, boolean internal) {
+	/**
+	 * Acquires the swap file object output stream on selected medium.
+	 * 
+	 * @param id - file id
+	 * @param internal - true for internal storage, otherwise external
+	 * @return the swap file object output stream on selected medium
+	 * @throws FileNotFoundException
+	 */
+	public static ObjectOutputStream getObjectOutputStream(long id, boolean internal) throws IOException {
 		FileOutputStream fos = getFileOutputStream(id, internal);
-		if (fos != null) {
-			try {
-				return new ObjectOutputStream(fos);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
-		else {
-			return null;
-		}
+		return new ObjectOutputStream(fos);
+	}
+	
+	/**
+	 * Acquires the swap file object input stream on internal storage.
+	 * 
+	 * @param id - file id
+	 * @return the swap file object input stream on internal storage
+	 * @throws FileNotFoundException
+	 */
+	public static ObjectInputStream getObjectInputStream(long id) throws IOException {
+		return getObjectInputStream(id, true);
 	}
 
-	public static ObjectInputStream getObjectInputStream(long id, boolean internal) {
+	/**
+	 * Acquires the swap file object input stream on selected medium.
+	 * 
+	 * @param id - file id
+	 * @param internal - true for internal storage, otherwise external
+	 * @return the swap file object input stream on selected medium
+	 * @throws FileNotFoundException
+	 */
+	public static ObjectInputStream getObjectInputStream(long id, boolean internal) throws IOException {
 		FileInputStream fis = getFileInputStream(id, internal);
-		if (fis != null) {
-			try {
-				return new ObjectInputStream(fis);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
-		} else {
-			return null;
-		}
+		return new ObjectInputStream(fis);
 	}
 
 
+	/**
+	 * Checks whether external storage is available.
+	 * 
+	 * @return true if external storage is available, otherwise false
+	 */
 	public static boolean isExternalStorageWritable() {
 		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()))
 			return true;
