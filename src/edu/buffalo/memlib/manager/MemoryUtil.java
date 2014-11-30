3a7228be90af8c1bc0602bbd1d4ebeb7be49adf8
Alexander Simeonov <agsimeon@buffalo.edu>
2014-11-29 23:29:57 -0500

    Moved MemUtil methods to MemoryUtil

--- src/edu/buffalo/memlib/manager/MemoryUtil.java
+++ src/edu/buffalo/memlib/manager/MemoryUtil.java
@@ -1,7 +1,7 @@
 package edu.buffalo.memlib.manager;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,6 +10,9 @@ import java.util.Scanner;
 import java.util.Vector;
 
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.content.Context;
 import android.util.Log;
 
 public final class MemoryUtil {
@@ -39,6 +42,8 @@ public final class MemoryUtil {
 	
 	
 	static int warningAt = 1000; /**we can adjust this for early warnings*/
+	
+	
 	
 	/**interface for receiving the current memory state*/
 	public static int getMemoryState()
@@ -140,6 +145,76 @@ public final class MemoryUtil {
 		}
 		
 	}
+	
+	/** Maximum heap space allowed for this application. */
+	public static long getMaxHeap() {
+		return Runtime.getRuntime().maxMemory();
+	}
+
+	/** Current allocated heap space. */
+	public static long getCurrentHeap() {
+		return Runtime.getRuntime().totalMemory();
+	}
+
+	/** Current used heap space. */
+	public static long getUsedHeap() {
+		return getCurrentHeap() - Runtime.getRuntime().freeMemory();
+	}
+
+	/** Total system memory. */
+	public static long getTotalMem() {
+		return scanProcForField("/proc/meminfo", "MemTotal");
+	}
+
+	/** Available system memory. */
+	public static long getFreeMem() {
+		return scanProcForField("/proc/meminfo", "MemFree");
+	}
+	
+	
+	/**
+	 * Check if the application is backgrounded.
+	 * 
+	 * @return true if backgrounded, otherwise false
+	 */
+	public static boolean isBackgrounded() {
+		File file = new File("/proc/" + android.os.Process.myPid() + "/oom_adj");
+		long oom_adj = -1;
+		
+		try {
+			Scanner scanner = new Scanner(file);
+			oom_adj = scanner.nextLong();
+			scanner.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return oom_adj != 0 ? true : false;
+	}
+
+	/** 
+	 *  The available memory on the system. This number should not be considered absolute: due 
+	 *  to the nature of the kernel, a significant portion of this memory is actually in use and 
+	 *  needed for the overall system to run well.
+	 */
+	public static long getAvailableMem(Context context) {
+		ActivityManager activityManager =  (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		MemoryInfo memoryInfo = new MemoryInfo();
+		activityManager.getMemoryInfo(memoryInfo);
+		return memoryInfo.availMem;
+	}
+
+	/** 
+	 *  The threshold of availMem at which we consider memory to be low and start killing 
+	 *  background services and other non-extraneous processes. 
+	 */
+	public static long getThreshold(Context context) {
+		ActivityManager activityManager =  (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		MemoryInfo memoryInfo = new MemoryInfo();
+		activityManager.getMemoryInfo(memoryInfo);
+		return memoryInfo.threshold;
+	}
 
 	private static long scanProcForField(String path, String field) {
 		File file = new File(path);
@@ -152,10 +227,10 @@ public final class MemoryUtil {
 				String token = scanner.next();
 				if (token.equals(field))
 					return scanner.nextLong(); 
-				String s = scanner.nextLine();
+				scanner.nextLine();
 			}
 		} catch (Exception e) {
-			Log.e("MemUtil", "Error scanning " + file + " for " + field, e);
+			Log.e("MemoryUtil", "Error scanning " + file + " for " + field, e);
 		} finally {
 			if (scanner != null)
 				scanner.close();
